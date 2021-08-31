96e2caff614dd2be6eef577c4d33fad1a758d8a9
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 23:34:56 +0530

    Iconable

---
+++ Iconabledemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+
+namespace Iconabledemo
+{
+    class Car : ICloneable
+    {
+        int width;
+
+        public Car(int width)
+        {
+            this.width = width;
+        }
+
+        public object Clone()
+        {
+            return new Car(this.width);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Width of car = {0}", this.width);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Car carOne = new Car(1695);
+            Car carTwo = carOne.Clone() as Car;
+
+            Console.WriteLine("{0}mm", carOne);
+            Console.WriteLine("{0}mm", carTwo);
+
+
+
+        }
+    }
+}
